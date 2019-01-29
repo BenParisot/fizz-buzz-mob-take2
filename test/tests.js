684e4b34ec9932f39a4c9684792a2b0d1a6c6424
Ben Parisot <me@benparisot.com>
2019-01-29 14:52:28 -0800

    clean up spaces in code

--- test/tests.js
+++ test/tests.js
@@ -4,10 +4,8 @@ function fizzBuzz(integer) {
     const wordOne = 'fizz';
     const wordTwo = 'buzz';
 
-    
     if(integer % 15 === 0) {
         result = wordOne + wordTwo;
-        
     }
     else if(integer % 5 === 0) {
         result = wordTwo;
@@ -20,12 +18,10 @@ function fizzBuzz(integer) {
     }
 
     return result;
-
 }
 
 
-test('is it divisible by 15', function(assert) {
-    
+test('is it divisible by 15', function(assert) {    
     const integer = 15;
     const expect = 'fizzbuzz';
     const result = fizzBuzz(integer);
@@ -33,21 +29,20 @@ test('is it divisible by 15', function(assert) {
 });
 
 test('is it divisible by 5', function(assert) {
-    
     const integer = 5;
     const expect = 'buzz';
     const result = fizzBuzz(integer);
     assert.equal(expect, result);
 });
-test('is it divisible by 3', function(assert) {
-    
+
+test('is it divisible by 3', function(assert) {    
     const integer = 3;
     const expect = 'fizz';
     const result = fizzBuzz(integer);
     assert.equal(expect, result);
 });
-test('not divisible by 5, 3, or 15', function(assert) {
-    
+
+test('not divisible by 5, 3, or 15', function(assert) {    
     const integer = 7;
     const expect = integer;
     const result = fizzBuzz(integer);
